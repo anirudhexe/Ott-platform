ccdd64b9d760056bfcf4681691c5bab3f710fb83
Archisman Mukherjee <119628042+Archisman02@users.noreply.github.com>
2024-04-10 21:57:40 +0530

    DB connected and models created

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("Mongo DB is connected!")).catch((err) => (console.log(err)));
 
 app.listen(8800, () => {
     console.log("Backend server is running!")
