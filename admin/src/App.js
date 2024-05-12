1f4ad0c878a4c0ba2afa0f2884653d376066c525
Archisman Mukherjee <119628042+Archisman02@users.noreply.github.com>
2024-05-12 20:01:20 +0530

    Context added, jwt login partially done

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -10,10 +10,11 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
-import { Navigate } from 'react-router-dom';
+import { AuthContext } from "./context/authContext/AuthContext";
+import { useContext } from "react";
 
 function App() {
-  const { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
   return (
     <Router>
       <Topbar />
@@ -21,16 +22,16 @@ function App() {
       <Sidebar />
         <Routes>
           <Route exact path="/" element={<Home />}/>
-          {/* <Route path="/login" element={<Login />}/> */}
-          <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+          {/* <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} /> */}
+          <Route path="/login" element={<Login />}/>
           <Route path="/users" element={<UserList />}/>
           <Route path="/user/:userId" element={<User />}/>
           <Route path="/newUser" element={<NewUser />}/>
           <Route path="/movies" element={<ProductList />}/>
           <Route path="/product/:productId" element={<Product />}/>
           <Route path="/newproduct" element={<NewProduct />}/>
-        </Routes>
-      </div>
+          </Routes>
+        </div>
     </Router>
   );
 }
